af0dee40aa05229f426b2239c1ab4e46552d1228
Isaacnewton88 <kenyeres.akos@gmail.com>
2020-09-18 18:31:54 +0200

    Item scripts beta

---
+++ Assets/MyScripts/InteractionObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionObject : MonoBehaviour
+{
+    public void DoInteraction()
+    {
+        //Do interaction
+        gameObject.SetActive(false);
+
+        //play sound
+        //Display rewards https://answers.unity.com/questions/1320381/is-it-possible-to-create-text-string-particles.html
+        //update character sheet
+    }
+}
---
+++ Assets/MyScripts/Quest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest : MonoBehaviour
+{
+    
+    // With interaction check if requirements met
+
+    //if not
+        //display requirements
+        //disable enter trigger for 5 sec
+
+    //if yes
+        //display rewards
+        //display completed item/art
+
+}
