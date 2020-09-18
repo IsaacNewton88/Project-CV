7ea2bb79ebb6bd4abce843947c242f474586ead2
Isaacnewton88 <kenyeres.akos@gmail.com>
2020-09-18 18:59:06 +0200

    script descriptions

---
+++ Assets/MyScripts/Character Sheet/Inventory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    //list of items with !check arnodls skeleton model first!
+        // name
+        // isAvailable
+        // display icon when available
+
+    /* Probable items:
+     * Armor
+     * Wizard Hat or Staff
+     * Sword??
+     * VR glasses
+     * Scientist labcoat 
+     */
+
+    // Display structure panel N slots
+
+    // Connection to Character animator to put on equipment when found
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/MyScripts/Character Sheet/Skills.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skills : MonoBehaviour
+{
+
+    // List of skills with:
+        // name
+        // int level
+
+    // Needs some kind of Display
+    /* display background human in T shape
+     * skills are organized from the middle circularry descending order by their lvl
+     * opacity 50% for start over background, bar grows by level
+     * should look like nomral distribution if skill levels are well balanced
+     */
+
+    /* current example skills
+     * Computer
+     * Gaming
+     * English
+     * Testing
+     * Leadership/Mentoring
+     * Planning
+     * Sciences (math, physics, etc)
+     * Autonomity (self learn)
+     * Teamwork
+     * Vision/Ideas/Perception??? some good namne for this
+     * Intuition // Innovation ??
+     * Problem Solving
+     * Design
+     * Creativity
+     * Cloud
+     * Linux
+     * C#
+     * Unity
+     * Jira
+     * Analytics
+     */
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
