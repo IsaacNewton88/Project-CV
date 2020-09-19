72ecafb960a60dd1979bcc8cd57e4d281c25de64
Isaacnewton88 <kenyeres.akos@gmail.com>
2020-09-19 20:48:43 +0200

    start of basic game elementy scripts

---
+++ Assets/MyScripts/Popup texts/TextPopup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextPopup : MonoBehaviour
+{
+    public static TextPopup Create(Vector3 position, string text)
+    {
+        Transform textPopupTransform = Instantiate(GameAssets.i.pfTexturePopup, position, Quaternion.identity);
+
+        TextPopup textPopup = textPopupTransform.GetComponent<TextPopup>();
+        textPopup.Setup("text");
+
+        return textPopup;
+    }
+    private TextMeshPro textMesh;
+
+    private void Awake()
+    {
+        textMesh = transform.GetComponent<TextMeshPro>();
+    }
+
+    public void Setup(string text)
+    {
+        textMesh.SetText(text);
+    }    
+}
