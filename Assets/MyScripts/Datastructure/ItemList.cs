fb63e467961b15598434c142436ac350f228ce88
Isaacnewton88 <kenyeres.akos@gmail.com>
2020-10-07 23:31:31 +0200

    Scriptableobject skill database + Bolt skill intantiator

---
+++ Assets/MyScripts/Datastructure/ItemList.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item List", menuName = "Item List")]
+public class ItemList : ScriptableObject
+{
+    public string _name;
+    public bool isOwned = false;
+    public Sprite icon;
+}
---
+++ Assets/MyScripts/Datastructure/SkillSet.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Skill Set", menuName = "Skill Set")]
+public class SkillSet : ScriptableObject
+{
+    public string _name;
+    public int level;
+    public Color color;    
+}
