0d1f49620c2c449e4477ebb74bacabec647a37ac
Isaacnewton88 <kenyeres.akos@gmail.com>
2020-10-09 18:59:48 +0200

    Interobject, item check, skill check, reward handling

--- Assets/MyScripts/Datastructure/ItemList.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "Item List", menuName = "Item List")]
-public class ItemList : ScriptableObject
-{
-    public string _name;
-    public bool isOwned = false;
-    public Sprite icon;
-}
---
+++ Assets/MyScripts/Datastructure/Requirement.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Requirenent")]
+public class Requirement : ScriptableObject
+{
+    public string _name;
+    public int level;       
+}
