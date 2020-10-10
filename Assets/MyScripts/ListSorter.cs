d1e58319ac0695fada7a60da730f3e8dcc86dec4
Isaacnewton88 <kenyeres.akos@gmail.com>
2020-10-10 17:09:43 +0200

    ListSorter

---
+++ Assets/MyScripts/ListSorter.cs
@@ -0,0 +1,31 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using MiscUtil.Collections.Extensions;
+using Bolt;
+using Ludiq;
+
+public class ListSorter : MonoBehaviour
+{
+    public enum byVariable { byNameDescending, byLevelDescending }
+    private List<GameObject> sortedList;
+
+    public List<GameObject> SortListBy(byVariable By, List<GameObject> objectList)
+    {
+        if (By == byVariable.byNameDescending)
+        {
+            sortedList = objectList.OrderByDescending(o => o.name.Length).ToList();
+        }
+        if (By == byVariable.byLevelDescending)
+        {
+            sortedList = System.Array.Sort(objectList, delegate(GameObject skill1, GameObject skill2)
+            {
+                skill1.GetType();
+            });
+        }
+        return sortedList;
+    }
+
+}*/
+
